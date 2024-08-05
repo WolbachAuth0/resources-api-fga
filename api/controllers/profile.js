8ff806502a99bb0d8077b3820fc76beffb10b050
WolbachAuth0 <aaron.wolbach@okta.com>
2024-08-05 15:39:58 -0400

    add orgs, roles and profile control

---
+++ api/controllers/profile.js
@@ -0,0 +1,29 @@
+const responseFormatter = require('./../middleware/responseFormatter')
+
+const User = require('./../models/User')
+
+module.exports = {
+  // User Endpoints
+  getUserProfile,
+  updateProfile,
+}
+
+// User Endpoints
+async function getUserProfile (req, res, next) {
+  const id = req.params.user_id
+
+  const user = new User({ id })
+  const payload = await user.getProfile()
+  const json = responseFormatter(req, res, payload)
+  res.status(payload.status).json(json)
+}
+
+async function updateProfile (req, res, next) {
+  const id = req.params.user_id
+  const body = req.body
+
+  const user = new User({ id })
+  const payload = await user.updateProfile({ body })
+  const json = responseFormatter(req, res, payload)
+  res.status(payload.status).json(json)
+}
