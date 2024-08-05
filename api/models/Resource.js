ae2571d323da85ea78daf25d2f4ed5f6e7234e7b
WolbachAuth0 <aaron.wolbach@okta.com>
2024-08-05 15:39:21 -0400

    update Resource class and routes

--- api/models/Resource.js
+++ api/models/Resource.js
@@ -1,95 +1,139 @@
-const axios = require('axios')
-const errorHandler = require('./ErrorHandler')
+const storage = [
+  {
+    resource_id: 1,
+    title: 'Document #1',
+    text: 'Never Forget that no military leader ever became great without audacity.',
+    author: 'Carl von Clausewitz'
+  },
+  {
+    resource_id: 2,
+    title: 'Document #2',
+    text: 'Courage, above all things, is the first quality of a warrior.',
+    author: 'Carl von Clausewitz'
+  },
+  {
+    resource_id: 3,
+    title: 'Document #3',
+    text: 'You cannot escape the responsibility of tomorrow by evading it today.',
+    author: 'Abraham Lincoln'
+  },
+  {
+    resource_id: 4,
+    title: 'Document #4',
+    text: 'Give me six hours to chop down a tree and I will spend the first four sharpening the axe.',
+    author: 'Abraham Lincoln'
+  },
+  {
+    resource_id: 5,
+    title: 'Document #5',
+    text: 'The supreme art of war is to subdue the enemy without fighting.',
+    author: 'Sun Tzu'
+  },
+  {
+    resource_id: 6,
+    title: 'Document #6',
+    text: 'Invincibility lies in defense; The possibility of victory in the attack.',
+    author: 'Sun Tzu'
+  },
+  {
+    resource_id: 7,
+    title: 'Document #7',
+    text: "Freedom is never more than one generation away from extinction. We didn't pass it to our children in the bloodstream. It must be fought for, protected, and handed on to them to do the same.",
+    author: 'Ronald Reagan'
+  },
+  {
+    resource_id: 8,
+    title: 'Document #8',
+    text: 'Peace is not the absence of conflict. It is the ability to handle conflict by peaceful means.',
+    author: 'Ronald Reagan'
+  }
+]
+
+const examples = [
+  {
+    "text": "Only two things are infinite, the universe and human stupidity and I'm not sure about the former.",
+    "author": "Albert Einstein"
+  },
+  {
+    "text": "Strive not to be a success, but rather to be of value.",
+    "author": "Albert Einstein"
+  }
+]
 
 class Resource {
 
-  constructor ({ resource = 'posts' }) {
-    // resource options are posts, comments, albums, photos
-    const request = {
-      baseURL: `https://jsonplaceholder.typicode.com/${resource}`,
-      headers: {
-        'Content-Type': 'application/json; charset=UTF-8',
-        'Accept': 'application/json'
-      }
-    }
-    this.config = request
-    this.http = axios.create(this.config)
+  constructor () {
+    this.store = storage
   }
 
-  async list (query) {
-    try {
-      const response = await this.http.get('/')
-      const payload = {
-        status: 200,
-        message: `Listed all resources`,
-        data: response.data
-      }
-      return payload
-    } catch (error) {
-      console.log(error)
-      return errorHandler(error)
-    }
+  get nextId () {
+    return Math.max(...this.store.map(x => x.resource_id)) + 1
   }
 
-  async getById ({ resource_id }) {
-    try {
-      // TODO: handle resource not found
-      const response = await this.http.get(`/${resource_id}`)
-      const payload = {
-        status: 200,
-        message: `Fetched resource with id ${resource_id}`,
-        data: response.data
-      }
-      return payload
-    } catch (error) {
-      console.log(error)
-      return errorHandler(error)
+  list (query) {
+    // TODO: deal with queries
+    const payload = {
+      status: 200,
+      message: `Listed all resources`,
+      data: this.store
     }
+    return payload
   }
 
-  async create (body) {
-    try {
-      const response = await this.http.post(`/`, body)
-      const payload = {
-        status: 201,
-        message: `Created resource with id ${response.data.id}`,
-        data: response.data
-      }
-      return payload
-    } catch (error) {
-      console.log(error)
-      return errorHandler(error)
+  getById ({ resource_id }) {
+    const resource = this.store.find(x => x.resource_id == resource_id)
+    const payload = {
+      status: resource ? 200 : 404,
+      message: resource ? `Fetched resource with id ${resource_id}` : `Resource with id ${resource_id} not found.`,
+      data: resource
     }
+    return payload
   }
 
-  async update ({ resource_id }, body) {
-    try {
-      const response = await this.http.put(`/${resource_id}`, body)
-      const payload = {
-        status: 200,
-        message: `Updated resource with id ${response.data.id}`,
-        data: response.data
-      }
-      return payload
-    } catch (error) {
-      console.log(error)
-      return errorHandler(error)
+  create ({ text, author }) {
+    const resource_id = this.nextId
+    console.log(resource_id)
+    const resource = {
+      resource_id,
+      title: `Document #${resource_id}`,
+      text,
+      author
     }
+    this.store.push(resource)
+    const payload = {
+      status: 201,
+      message: `Created resource with id ${resource_id}`,
+      data: resource
+    }
+    return payload
   }
 
-  async remove ({ resource_id }) {
-    try {
-      const response = await this.http.delete(`/${resource_id}`)
-      const payload = {
-        status: 200,
-        message: `Deleted resource with id ${resource_id}`,
-        data: response.data
-      }
-      return payload
-    } catch (error) {
-      console.log(error)
-      return errorHandler(error)
+  update ({ resource_id }, { text, author }) {
+    const index = this.store.findIndex(x => x.resource_id == resource_id)
+    const resource = {
+      resource_id,
+      title: `Document #${resource_id}`,
+      text,
+      author
     }
+    this.store.splice(index, 1, resource)
+    const payload = {
+      status: 200,
+      message: `Updated resource with id ${resource_id}`,
+      data: resource
+    }
+    return payload
+  }
+
+  remove ({ resource_id }) {
+    const index = this.store.findIndex(x => x.resource_id == resource_id)
+    this.store.splice(index, 1)
+    const payload = {
+      status: 200,
+      message: `Deleted resource with id ${resource_id}`,
+      data: []
+    }
+    return payload
   }
 
 }
--- api/routes/resources.js
+++ api/routes/resources.js
@@ -9,35 +9,35 @@ const options = { failWithError: true }
 
 router
   .route('/')
-  .all(verifyJWT)
+  // .all(verifyJWT)
   .get(
     resources.list
   )
   .post(
-    checkJWTScopes(['create:resource'], options),
+    // checkJWTScopes(['create:resource'], options),
     // schemaValidator(),
     resources.create
   )
 
 router
   .route('/:resource_id')
-  .all(verifyJWT)                               // verify signature on access token
+  // .all(verifyJWT)                               // verify signature on access token
   .get(
-    checkJWTScopes(['read:resource'], options), // verify access token contains necessary permission(s)
+    // checkJWTScopes(['read:resource'], options), // verify access token contains necessary permission(s)
     resources.getById                           // execute the get User by Id function
   )
   .put(
-    checkJWTScopes(['update:resource'], options),
+    // checkJWTScopes(['update:resource'], options),
     // schemaValidator(),
     resources.update
   )
   .patch(
-    checkJWTScopes(['update:resource'], options),
+    // checkJWTScopes(['update:resource'], options),
     // schemaValidator(),
     resources.update
   )
   .delete(
-    checkJWTScopes(['delete:resource'], options),
+    // checkJWTScopes(['delete:resource'], options),
     resources.remove
   )
   
