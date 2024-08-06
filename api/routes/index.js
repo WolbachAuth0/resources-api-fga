14ab9e9a5ef87da071ef7e024676378fc127c579
WolbachAuth0 <aaron.wolbach@okta.com>
2024-08-06 09:25:31 -0400

    update environment var names

--- api/routes/index.js
+++ api/routes/index.js
@@ -34,11 +34,11 @@ router
   .get((req, res) => {
     const query = Object.assign(req.query, {
       response_type: 'code',
-      client_id: process.env.VUE_APP_AUTH0_CLIENT_ID,
-      redirect_uri: `${process.env.VUE_APP_DOMAIN}/profile`,
+      client_id: process.env.FRONTEND_AUTH0_CLIENT_ID,
+      redirect_uri: `${process.env.FRONTEND_DOMAIN}/profile`,
       response_mode: 'query'
     })
     const qs = new URLSearchParams(query).toString()
-    const to = `https://${process.env.VUE_APP_CUSTOM_DOMAIN}/authorize?${qs}`
+    const to = `https://${process.env.AUTH0_CUSTOM_DOMAIN}/authorize?${qs}`
     res.redirect(to)
   })
