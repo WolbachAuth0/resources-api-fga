8c387e96c9b69ff8b45963d4319c5e495cb43d6a
WolbachAuth0 <aaron.wolbach@okta.com>
2024-08-06 10:42:04 -0400

    test schema validator

--- scripts/test-schema-validator.js
+++ scripts/test-schema-validator.js
@@ -1,2 +1,31 @@
-const schemaValidator = require('./../api/middleware/schemaValidator')
+const Validator = require('jsonschema').Validator
+
 const { quotation } = require('./../api/controllers/resources').schemas
+
+const examples = [
+  {
+    "text": "Only two things are infinite, the universe and human stupidity and I'm not sure about the former.",
+    "author": "Albert Einstein"
+  },
+  {
+    "text": "Strive not to be a success, but rather to be of value.",
+    "author": "Albert Einstein"
+  }
+]
+
+const badExample = {
+  "test": 'not a good property',
+  'other': 'another field'
+}
+
+main()
+
+function main () {
+  const validator = new Validator()
+  const goodExample = examples[1]
+  const goodValidation = validator.validate(goodExample, quotation)
+  console.log(goodValidation.valid)
+
+  const badValidation = validator.validate(badExample, quotation)
+  console.log(badValidation.valid)
+}
