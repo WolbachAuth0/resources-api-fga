26441304a8c60d934858b9716169b181a631e184
WolbachAuth0 <aaron.wolbach@okta.com>
2024-09-08 13:19:32 -0400

    modify resource constructor

--- api/models/Resource.js
+++ api/models/Resource.js
@@ -1,60 +1,58 @@
 const { logger } = require('./logger')
 
-const storage = [
-  {
-    resource_id: 1,
-    title: 'Document #1',
-    text: 'Never Forget that no military leader ever became great without audacity.',
-    author: 'Carl von Clausewitz'
-  },
-  {
-    resource_id: 2,
-    title: 'Document #2',
-    text: 'Courage, above all things, is the first quality of a warrior.',
-    author: 'Carl von Clausewitz'
-  },
-  {
-    resource_id: 3,
-    title: 'Document #3',
-    text: 'You cannot escape the responsibility of tomorrow by evading it today.',
-    author: 'Abraham Lincoln'
-  },
-  {
-    resource_id: 4,
-    title: 'Document #4',
-    text: 'Give me six hours to chop down a tree and I will spend the first four sharpening the axe.',
-    author: 'Abraham Lincoln'
-  },
-  {
-    resource_id: 5,
-    title: 'Document #5',
-    text: 'The supreme art of war is to subdue the enemy without fighting.',
-    author: 'Sun Tzu'
-  },
-  {
-    resource_id: 6,
-    title: 'Document #6',
-    text: 'Invincibility lies in defense; The possibility of victory in the attack.',
-    author: 'Sun Tzu'
-  },
-  {
-    resource_id: 7,
-    title: 'Document #7',
-    text: "Freedom is never more than one generation away from extinction. We didn't pass it to our children in the bloodstream. It must be fought for, protected, and handed on to them to do the same.",
-    author: 'Ronald Reagan'
-  },
-  {
-    resource_id: 8,
-    title: 'Document #8',
-    text: 'Peace is not the absence of conflict. It is the ability to handle conflict by peaceful means.',
-    author: 'Ronald Reagan'
-  }
-]
-
 class Resource {
 
   constructor () {
-    this.store = storage
+    this.store = [
+      {
+        resource_id: 1,
+        title: 'Document #1',
+        text: 'Never Forget that no military leader ever became great without audacity.',
+        author: 'Carl von Clausewitz'
+      },
+      {
+        resource_id: 2,
+        title: 'Document #2',
+        text: 'Courage, above all things, is the first quality of a warrior.',
+        author: 'Carl von Clausewitz'
+      },
+      {
+        resource_id: 3,
+        title: 'Document #3',
+        text: 'You cannot escape the responsibility of tomorrow by evading it today.',
+        author: 'Abraham Lincoln'
+      },
+      {
+        resource_id: 4,
+        title: 'Document #4',
+        text: 'Give me six hours to chop down a tree and I will spend the first four sharpening the axe.',
+        author: 'Abraham Lincoln'
+      },
+      {
+        resource_id: 5,
+        title: 'Document #5',
+        text: 'The supreme art of war is to subdue the enemy without fighting.',
+        author: 'Sun Tzu'
+      },
+      {
+        resource_id: 6,
+        title: 'Document #6',
+        text: 'Invincibility lies in defense; The possibility of victory in the attack.',
+        author: 'Sun Tzu'
+      },
+      {
+        resource_id: 7,
+        title: 'Document #7',
+        text: "Freedom is never more than one generation away from extinction. We didn't pass it to our children in the bloodstream. It must be fought for, protected, and handed on to them to do the same.",
+        author: 'Ronald Reagan'
+      },
+      {
+        resource_id: 8,
+        title: 'Document #8',
+        text: 'Peace is not the absence of conflict. It is the ability to handle conflict by peaceful means.',
+        author: 'Ronald Reagan'
+      }
+    ]
   }
 
   static get schema () {
