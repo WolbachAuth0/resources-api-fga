adcc063a72736f7a0a87382415bb62db3df88d78
WolbachAuth0 <aaron.wolbach@okta.com>
2024-09-10 19:52:53 -0400

    debug authentication api client constructor

--- api/models/authenticationAPI.js
+++ api/models/authenticationAPI.js
@@ -6,20 +6,16 @@
  * @returns {Object} Instance of Management API Client
  */
 const { AuthenticationClient } = require('auth0')
-try {
-  const options = {
-    domain: process.env.AUTH0_CUSTOM_DOMAIN,
-    clientId: process.env.FRONTEND_AUTH0_CLIENT_ID,
-    clientSecret: process.env.FRONTEND_AUTH0_CLIENT_SECRET,
-    audience: process.env.AUDIENCE,
-    // scope: scopes.join(' ')
-  }
-  const AuthenticationClient = require('auth0').AuthenticationClient
-  const authentication = new AuthenticationClient(options)
-} catch (err) {
-  console.log('An error occurred while instantiating authentication api client.')
-  throw err
+
+const options = {
+  domain: process.env.AUTH0_CUSTOM_DOMAIN,
+  clientId: process.env.FRONTEND_AUTH0_CLIENT_ID,
+  clientSecret: process.env.FRONTEND_AUTH0_CLIENT_SECRET,
+  audience: process.env.AUDIENCE,
+  // scope: scopes.join(' ')
 }
+const authentication = new AuthenticationClient(options)
+
 
 // Documentation
 // https://auth0.github.io/node-auth0/classes/auth.AuthenticationClient.html
