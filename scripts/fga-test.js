987b455d7c828accf87a520b51ab073d85596e78
WolbachAuth0 <aaron.wolbach@okta.com>
2024-09-10 15:31:40 -0400

    add a sanity check script

--- scripts/fga-test.js
+++ scripts/fga-test.js
@@ -7,5 +7,13 @@ const fga = require('./../api/models/FGA')
 main()
 
 async function main () {
-  await fga.testMe()
+  const user_id = 'auth0|66b171a53141b01fb58990f5'
+  const resource_id = '1'
+  const options = {
+    user: `user:${user_id}`,
+    object: `doc:${resource_id}`
+  }
+
+  const relations = await fga.listRelations(options)
+  console.log(relations)
 }
